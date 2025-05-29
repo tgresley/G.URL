63fe5434f5b015f90c5db8119f25f8ea09630db7
Tess <boogaloon1@gmail.com>
2025-05-29 15:06:09 +1000

    Initial commit

---
+++ amipretty/G.URL/manicpixie/sketch.js
@@ -0,0 +1,90 @@
+    const sadgirlResults = [
+      {
+        title: "paprika",
+        text: "You aren't mentally ill, you are just manic-pixie dream girl, literally. <br> <b>age</b>: 26 <br>  <b>sex</b>: g.url <br>  <b>location </b>: near-future, Japan <br>  <b>favourite animal</b>: butterfly <br>  <b>interests</b>: entering peoples dreams, flying, making men fall in love with you",
+        img: "./PFP/PAPRIKA_PFP.png",
+        bg: "./bg/PAP_BG.jpg",
+        pfp: "./PFP/PAPRIKA_PFP.png",
+        audio: "./audio/PAPRIKA_SONG.mp3",
+        album: "./albums/PAPRIKA.jpg",
+        caption: "Paprika OST - Parade",
+        interaction: "<button onclick='alert(\"You entered a stranger’s REM cycle.\")'>Enter dream</button>"
+      },
+      {
+        title: "layla",
+        text: "You think mean boys need love too <3. <br> <b>age</b>: 16 <br>  <b>sex</b>: g.url <br>  <b>location </b>: Buffalo, New York <br>  <b>favourite animal</b>: butterfly <br>  <b>interests</b>: roses, male projection",
+        img: "honeymoon.jpg",
+        bg: "./BG/B66_BG.jpg",
+        pfp: "./PFP/B66_PFP.png",
+        audio: "./audio/B66_SONG.mp3",
+        album: "./albums/BF.jpg",
+        caption: "Layla - Love Is Hell",
+        interaction: "<button onclick='alert(\"You just cried on the plane ride home.\")'>Cry again</button>"
+      },
+      {
+        title: "angela hayes",
+        text: "You could seduce your best friends dad. <br> <b>age</b>: 16 <br>  <b>sex</b>: g.url <br>  <b>location </b>: the suburbs, California <br>  <b>favourite animal</b>: butterfly <br>  <b>interests</b>: roses, the male gaze and insecure attachment",
+        img: "dadcrush.jpg",
+        bg: "./bg/AB_BG.webp",
+        pfp: "./pfp/AB_PFP.png",
+        audio: "./audio/AB_SONG.mp3",
+        album: "./albums/AB.jpg",
+        caption: "Angela Hayes - American Beauty",
+        interaction: "<button onclick='alert(\"You asked your friend's dad for a ride.\")'>Flirt</button>"
+      },
+      {
+        title: "dolores hayes 'lolita'",
+        text: "You are such a #nymphette. <br> <b>age</b>: 14 <br>  <b>sex</b>: g.url <br>  <b>location </b>: small town, America <br>  <b>favourite animal</b>: butterfly <br>  <b>interests</b>: candy, cheekiness and roadtrips",
+        img: "milfhunter.jpg",
+        bg: "./BG/LOLITA_BG.jpg",
+        pfp: "./PFP/LOLITA_PFP.png",
+        audio: "./audio/LOLITA_SONG.mp3",
+        album: "./albums/LOLITA.jpg",
+        caption: "Lolita Soundtrack - Love Theme",
+        interaction: "<button onclick='alert(\"You just DM’d a 45-year-old actor.\")'>Send DM</button>"
+      },
+      {
+        title: "christiane.f",
+        text: "You started a heroin addiction for a boy. <br> <b>age</b>: 14 <br>  <b>sex</b>: g.url <br>  <b>location </b>: Zoo Station, West Berlin <br>  <b>favourite animal</b>: cat <br>  <b>interests</b>: david bowie, heroin, plastic bags and vinyl collections",
+        img: "neolib.jpg",
+        bg: "./BG/CF_BG.jpg",
+        pfp: "./PFP/CF_PFP.png",
+        audio: "./audio/CF_SONG.mp3",
+        album: "./albums/CF.jpg",
+        caption: "David Bowie - Heroes (live)",
+        interaction: "<button onclick='alert(\"WIR KINDER VOM BAHNHOF ZOO\")'>Take a break</button>"
+      },
+      {
+        title: "lilja",
+        text: "Your mum left you in the soviet housing estate. <br> <b>age</b>: 14 <br>  <b>sex</b>: g.url <br>  <b>location </b>: unknown, Soviet Union <br>  <b>favourite animal</b>: bird <br>  <b>interests</b>: vodka, angel wings, friendship",
+        img: "soviet.jpg",
+        bg: "./BG/L4E_BG.jpg",
+        pfp: "./PFP/L4E_PFP.png",
+        audio: "./audio/L4E_SONG.mp3",
+        album: "./albums/L4E.jpg",
+        caption: "Lilja 4-ever - Soundtrack",
+        interaction: "<button onclick='alert(\"You just read a Gulag memoir on the bus.\")'>Read more</button>"
+      },
+      {
+        title: "lux lisbon",
+        text: "Your gang small but y'all crazy. <br> <b>age</b>: 14 <br>  <b>sex</b>: g.url <br>  <b>location </b>: Grosse Pointe, Michigan <br>  <b>favourite animal</b>: cat <br>  <b>interests</b>: cigarettes, being mysterious, defying catholicism",
+        img: "pact.jpg",
+        bg: "./BG/VS_BG.jpeg",
+        pfp: "./PFP/VS_PFP.png",
+        audio: "./audio/VS_SONG.mp3",
+        album: "./albums/VS.png",
+        caption: "Air - Playground Love",
+        interaction: "<button onclick='alert(\"You scheduled a moonlit ritual.\")'>Swear pact</button>"
+      },
+      {
+        title: "daisy randone",
+        text: "You only eat chicken and laxatives. <br> <b>age</b>: 18 <br>  <b>sex</b>: g.url <br>  <b>location </b>: Grosse Pointe, Michigan <br>  <b>favourite animal</b>: chicken <br>  <b>interests</b>: cigarettes, being mysterious, defying catholicism",
+        img: "pact.jpg",
+        bg: "./BG/GI_BG.jpg",
+        pfp: "./PFP/G1_PFP.png",
+        audio: "./audio/GI_SONG.mp3",
+        album: "./albums/GI.jpg",
+        caption: "Girl, Interrupted OST",
+        interaction: "<button onclick='alert(\"You scheduled a moonlit ritual.\")'>Swear pact</button>"
+      }
+    ];
